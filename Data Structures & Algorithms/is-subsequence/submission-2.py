fb8fb73c5ecae37098c47c9e914700bac9dabc60
Aniruddha Joshi <aniruddha.jaguar@gmail.com>
2026-04-07 08:58:43 -0400

    Add: is-subsequence - submission-2

---
+++ Data Structures & Algorithms/is-subsequence/submission-2.py
@@ -0,0 +1,8 @@
+class Solution:
+    def isSubsequence(self, s, t):
+        pos = 0
+        for i in range(len(s)):
+            if t.find(s[i],pos) == -1:
+                return False
+            pos = t.find(s[i],pos) +1
+        return True
